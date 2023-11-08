c8a349c3267a833700a23b2e1bbf9b12604f0aed
danielcaicedo98 <daniel.andres.caicedo@correounivalle.edu.co>
2023-11-08 13:36:50 -0500

    creando archivo escritura parametros

--- app.py
+++ app.py
@@ -1,42 +1,62 @@
 from flask import Flask, render_template, request, send_file
-import minizinc
 import os
 
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
+
 def index():
     if request.method == "POST":
-        archivo = request.files["archivo"]
+        archivo = request.files["archivo"] 
         if archivo:
-            # Copia el archivo a un nuevo nombre para evitar conflictos
-            archivo_nombre = archivo.filename
-            archivo_copia_nombre = f"temp_{archivo_nombre}"
-            archivo.save(archivo_copia_nombre)
+            contenido = archivo.read().decode('utf-8')            
+            lines = contenido.split("\n")
+            archivo_salida = "parametros.dzn"
+            try:  
+                print(lines)
+                # Leer J y K de las dos primeras líneas
+                J = int(lines[0].strip())
+                K = int(lines[1].strip())
+                # Leer el resto de las líneas y dividirlas en grupos de 3
+                #grupos = [lines[i:i + 3] for i in range(2, len(lines), 3)]
+                print(lines[3].split(","))
+                # Crear un diccionario para mapear las letras a las listas
+                diccionario = {
+                    'E': [],
+                    'A': [],
+                    'G': [],
+                    'F': [],
+                    'V': [],
+                    'piso': [],
+                    'techo': [],
+                    'Sup': [],
+                    'Inf': [],
+                    'P': [],
+                    'D': [],
+                    'R': []
+                }
 
-            # Crea un entorno MiniZinc
-            gecode = minizinc.Solver.lookup("coin-bc")
+                #for i, grupo in enumerate(grupos):
+                for i in range(2,14):
+                    letra = list(diccionario.keys())[i-2]
+                    valores = [float(x) for x in lines[i].split(',')]
+                    diccionario[letra] = valores
 
-            # Carga el modelo MiniZinc desde el archivo copiado
-            model = minizinc.Model(archivo_copia_nombre)
+                # Abrir el archivo de salida para escritura
+                with open(archivo_salida, 'w') as f:
+                    f.write(f'J = {J};\n')
+                    f.write(f'K = {K};\n')
+                    for letra, valores in diccionario.items():
+                        f.write(f'{letra} = {valores};\n')
 
-            # Crea una instancia del modelo
-            instance = minizinc.Instance(gecode, model)
+                print(f'Procesamiento completado. Resultados escritos en {archivo_salida}')
 
-            # Resuelve la instancia
-            result = instance.solve()
-
-            # Comprueba si se encontró una solución
-            if result:
-                resultado = "Solución encontrada:<br>" + str(result)
-            else:
-                resultado = "No se encontró solución."
-
-            # Borra el archivo copiado
-            os.remove(archivo_copia_nombre)
-
-            return resultado
+            except FileNotFoundError:
+                print("El archivo de entrada no existe.")
+            except Exception as e:
+                print(f"Ocurrió un error: {e}")    
+        #return 
 
     return render_template("index.html")
 
